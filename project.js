731c764212efc4177724b1e52cc9028e00997b2b
syedusama <usamasyed237@gmail.com>
2024-09-15 20:04:22 +0500

    adding game

--- project.js
+++ project.js
@@ -17,7 +17,6 @@ gsap.to(".nav",{
     scrollTrigger:{
         trigger:".nav",
         scroll:"body",
-        // markers:true,
         start: "top -10%",
         end:"top -11%",
         scrub:1
@@ -32,5 +31,61 @@ gsap.to(".main", {
         start:"top -30%",
         end:"top -100%",
         scrub:2
+    }
+})
+
+function makeBubbl(){
+    var clutter = '';
+    for(let i = 1; i<=108; i++)
+    {
+        let ranNum = Math.floor(Math.random()*10);
+        clutter += `<div class="bubble">${ranNum}</div>`;
+        // clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
+    }
+    
+    document.querySelector(".pannel_bottom").innerHTML = clutter
+}
+makeBubbl();
+
+var timer = 60;
+function setTimer(){
+    var timerInt = setInterval(function(){
+        if(timer>0){
+            timer--
+            document.querySelector("#setTime").textContent = timer
+        }
+        else{
+            clearInterval(timerInt);
+            document.querySelector(".pannel_bottom").innerHTML = `<h1 class="gameover">Game Over</h1>`
+            document.querySelector("#randomHit").textContent = 0
+            document.querySelector("#score").textContent = 0
+        }
+    },1000)
+}
+setTimer();
+
+var hitNum
+function hitVal(){
+    hitNum = Math.floor(Math.random()*10)
+    document.querySelector("#randomHit").textContent = hitNum
+}
+hitVal()
+
+var score = 0;
+function increaseScore(){
+    score += 10;
+    document.querySelector("#score").textContent = score
+}
+
+
+document.querySelector("#bubbleClick").addEventListener("click",function(dets){
+    let clickedNum = Number(dets.target.textContent);
+    if(clickedNum==hitNum){
+        increaseScore()
+        hitVal()
+        makeBubbl()
+    }
+    else{
+        document.querySelector("#score").textContent = 0
     }
 })
